1372d7c4191420adbfa2a2a915928d8d6404ea57
rosie w <rosie.wilt@gmail.com>
2020-04-08 16:44:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,8 +27,13 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
